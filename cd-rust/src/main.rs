d30d354b504d601e0af7ea0dd92d9a422d09109d
Geovane Guibes <geovaneguibes@gmail.com>
2023-06-05 21:52:30 +0100

    add tests

--- cd-rust/src/main.rs
+++ cd-rust/src/main.rs
@@ -12,18 +12,38 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let dir;
-
-    if args.dir == "" {
-        dir = std::env::var("HOME").unwrap_or("none".to_string());
-    } else {
-        dir = args.dir;
-    }
-
+    let dir = get_home_dir(&args.dir);
     let root = Path::new(&dir);
     assert!(env::set_current_dir(&root).is_ok());
     println!(
         "Successfully changed working directory to {}!",
         root.display()
     );
+}
+
+fn get_home_dir(dir: &str) -> String {
+    if dir != "" {
+        return dir.to_string();
+    }
+    let home_dir = std::env::var("HOME").unwrap_or("~".to_string());
+    return home_dir;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_home_dir() {
+        let test_dir = "/home/username";
+        let dir = get_home_dir(&test_dir);
+        assert_eq!(dir, test_dir.to_string());
+    }
+
+    #[test]
+    fn test_get_home_dir_empty() {
+        let dir = get_home_dir("");
+        let home_dir = std::env::var("HOME").unwrap_or("~".to_string());
+        assert_eq!(dir, home_dir);
+    }
 }
